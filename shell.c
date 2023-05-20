d94aa33be469abaa1ca3a10e1b8ad09683c1a246
mohamedibrahim135 <migif2003@gmail.com>
2023-05-20 14:46:41 -0700

    d

--- shell.c
+++ shell.c
@@ -8,38 +8,32 @@
 * Return: Always 0.
 */
 int main(void)
-{char *args[3];
+{
+char *args[2];
 int status;
 pid_t pid;
 char *command;
-char *end = "fail";
 size_t size = 100;
-int i = 0, j;
+int i = 0;
 while (1)
 {printf("#cisfun$ ");
+fflush(stdout);
 command = malloc(sizeof(char) * (size));
-getline(&command, &size, stdin);
+if (getline(&command, &size, stdin) == -1)
+{ break; }
 while (command[i] != '\n' && command[i] != '\0')
 { i++; }
 command[i] = '\0';
-j = 0;
-args[j] = command;
-for (i = 0; command[i] != '\0'; i++)
-{
-if (command[i] == ' ')
-{
-j++;
-args[j] = end;
-break; }}
-j++;
-args[j] = NULL;
+args[0] = command;
+args[1] = NULL;
 pid = fork();
 if (pid == 0)
-{execve(args[0], args, NULL);
+{execve(command, args, NULL);
 perror("./shell");
 exit(EXIT_FAILURE); }
 else if (pid < 0)
 { perror("fork"); }
 else
 { waitpid(pid, &status, 0); }}
+free(command);
 return (0); }
