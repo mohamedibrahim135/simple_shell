21e34b166093f6a948e2977d277ee29f9ced8981
mohamedibrahim135 <125568149+mohamedibrahim135@users.noreply.github.com>
2023-05-24 03:43:03 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -2,38 +2,47 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#define MAX_ARGS 10
 /**
 * main - PID
 * Return: Always 0.
 */
 int main(void)
 {
+char *command = NULL;
+size_t command_size = 0;
+ssize_t input_length;
 char *args[2];
-int status;
 pid_t pid;
-char *command;
-size_t size = 100;
-int i = 0;
 while (1)
-{printf("#cisfun$ ");
+{
+printf("#cisfun$ ");
 fflush(stdout);
-command = malloc(sizeof(char) * (size));
-if (getline(&command, &size, stdin) == -1)
-{ break; }
-while (command[i] != '\n' && command[i] != '\0')
-{ i++; }
-command[i] = '\0';
+input_length = getline(&command, &command_size, stdin);
+if (input_length == -1)
+{
+break;
+}
+if (command[0] == '\n')
+continue;
+command[input_length - 1] = '\0';
 args[0] = command;
 args[1] = NULL;
 pid = fork();
 if (pid == 0)
-{execve(command, args, NULL);
-perror("./shell");
-exit(EXIT_FAILURE); }
+{
+execve(command, args, NULL);
+perror("Error");
+exit(EXIT_FAILURE);
+}
 else if (pid < 0)
-{ perror("fork"); }
+{
+	perror("fork");
+}
 else
-{ waitpid(pid, &status, 0); }}
+{
+waitpid(pid, NULL, 0);
+}
+}
 free(command);
-return (0); }
+return (0);
+}
